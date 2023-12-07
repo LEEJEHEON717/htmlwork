dffdb311ebab77ebfa6b21e9a1c79acb5d396ebb
LEEJEHEON717 <leejehun0717@naver.com>
2023-12-07 18:20:44 +0900

    sub_project

--- cafe12/js/main.js
+++ cafe12/js/main.js
@@ -1,29 +1,28 @@
 
-window.onload = function (){
-  // 이미지 슬라이드
+window.onload = function(){
+  //이미지 슬라이드
+  let picture = ["images/header0.jpg", "images/header1.jpg",
+            "images/header2.jpg"];
+  let picIdx = 0;
 
-    let picture = ["images/header0.jpg", "images/header1.jpg",
-      "images/header2.jpg",];
-    let picIdx = 0;
+  showSlide();
 
-    showSlide();
-
-    function showSlide() {
-      //이미지 경로 - src 속성 사용
-      document.getElementById("pic").src = picture[picIdx];
-      picIdx += 1;
-      if (picIdx == picture.length) {
-        picIdx = 0;
-      }
-      setTimeout(showSlide, 3000);
+  function showSlide(){
+    //이미지 경로 - src 속성 사용
+    document.getElementById("pic").src = picture[picIdx];
+    picIdx += 1;
+    if(picIdx == picture.length){
+      picIdx = 0;
     }
+    setTimeout(showSlide, 3000);
+  }
 
-  // 시계 생성
+  //시계 생성
   setInterval(myWatch, 1000);
 
-  function myWatch() {
+  function myWatch(){
     const now = new Date(); //날짜와 시간 함께 생성
-    let time = now.toLocaleTimeString(); //시간만 생성
+    let time = now.toLocaleTimeString(); //시간만 출력
     console.log(time);
 
     let watch = document.getElementById("show")
