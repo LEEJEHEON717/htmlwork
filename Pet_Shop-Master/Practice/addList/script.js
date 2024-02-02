f8e791b9df0be847ac5fda9e585cc231f8d2219e
LEEJEHEON717 <YourGitHubEmail@example.com>
2024-02-02 18:27:30 +0900

    project

--- Pet_Shop-Master/Practice/addList/script.js
+++
@@ -1,8 +0,0 @@
-let list = document.getElementById("list");
-
-for(let i = 0; i < 5; i++) {
-    let li = document.createElement("li");
-    let img = document.createElement("img");
-    li.appendChild(img);
-    list.appendChild(li);
-}
